6393b45ea955979c90f466b2b69f7a9efcc56073
GGDuckGGuck <rutc0512@naver.com>
2023-12-14 02:43:03 +0900

    불필요 파일 삭제

--- Make_foggy.py
+++ Make_foggy.py
@@ -9,7 +9,7 @@ data = np.array(image)
 
 # 흰색 "안개" 레이어를 만듭니다. 이 레이어의 강도를 조절하여 안개의 밀도를 조절할 수 있습니다.
 white_layer = np.full((data.shape[0], data.shape[1], 3), [255, 255, 255], dtype=np.uint8)
-fog_intensity = 0.7  # 안개의 강도를 조절합니다 (0.0: 투명, 1.0: 완전히 흰색)
+fog_intensity = 0.7 # 안개의 강도를 조절합니다 (0.0: 투명, 1.0: 완전히 흰색)
 
 # 원본 이미지와 흰색 레이어를 결합합니다.
 foggy_image_data = data * (1 - fog_intensity) + white_layer * fog_intensity
